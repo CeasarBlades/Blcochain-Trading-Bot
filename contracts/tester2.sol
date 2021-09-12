4336f45e810ee562d81801de0fb98c18acdcc39b
ceasarblades <saula.atama@gmail.com>
2021-09-12 22:39:29 +1200

    logic added

--- contracts/tester2.sol
+++ contracts/tester2.sol
@@ -3,3 +3,9 @@ strategy(title- " Moving average average", oeverlay-true)
 // Definitions
 shortMa - sma(close, 20)
 longMa - sma(close, 50)
+
+// Logic
+longSignal - crossover(shortMa, longMa)
+shortMa - crossover(longMa, shortMa)
+
+//
