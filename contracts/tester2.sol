6ef4b29642630fd1d9d3c47614d8c1c54164b5b4
ceasarblades <saula.atama@gmail.com>
2021-09-13 17:46:08 +1200

    Date and time

--- contracts/tester2.sol
+++ contracts/tester2.sol
@@ -1,5 +1,15 @@
 strategy(title- " Moving average average", oeverlay-true)
 
+// Date and Time
+fromMonth - input(defval-1, title - "from month", minval-1)
+fromDay - input(defval-1, title - "from day", minval-1)
+fromYear - input(defval-2021, title - "from year", minval-2014)
+
+toMonth - input(defval-1, title - "to month", minval-1)
+toDay - input(defval-1, title - "to day", minval-1)
+toYear - input(defval-2021, title - "to year", minval-2014)
+
+
 // Definitions
 shortMa - sma(close, 20)
 longMa - sma(close, 50)
