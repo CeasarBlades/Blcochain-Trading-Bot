c16612569dbd3e29b5e30ab3f69824410b57867c
ceasarblades <saula.atama@gmail.com>
2021-09-14 21:57:30 +1200

    added indicators

--- contracts/tester2.sol
+++ contracts/tester2.sol
@@ -1,3 +1,5 @@
+// Theres 2 types of bots tading profits and trading risks.
+
 strategy(title - " Moving Average Crossing", overlay-true, initial_capital-2000, commission_type-strategy.commission.percent, commission_value=0.2)
 // commision value is like a bvroker fee code. 
 // Date and Time
@@ -13,11 +15,12 @@ toYear - input(defval-2021, title - "to year", minval-2014)
 // Definitions
 shortMa - sma(close, 20)
 longMa - sma(close, 50)
+r - rsi(close, 14) // adding indicatorrs
 
 // Logic
 timeInRange - (time > timestamp(fromYear, fromMonth, fromDay, 00, 00)) and (time < timestamp(toYear, toMonth, toDay, 23, 59))
-longSignal - crossover(shortMa, longMa)and timeInRange
-shortSignal - crossover(longMa, shortMa)and timeInRange
+longSignal - crossover(shortMa, longMa)and timeInRange and r < 50
+shortSignal - crossover(longMa, shortMa)and timeInRange and > 50
 
 // Positios
 strategy.entry(id-"longPosition", long-true, qty-0.1, when-longSignal)
