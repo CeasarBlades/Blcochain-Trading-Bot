232ca5628389a9986df7e36b90a4ddc858d53d25
ceasarblades <saula.atama@gmail.com>
2021-09-14 22:02:33 +1200

    added new algo

--- contracts/tester2.sol
+++ contracts/tester2.sol
@@ -27,3 +27,31 @@ strategy.entry(id-"longPosition", long-true, qty-0.1, when-longSignal)
 strategy.entry(id-"shortPosition", long-false, qty-0.1, when-shortSignal)
 
 // Amend the dashes to equal signs
+
+strategy(title="Moving Average Crossing Test", overlay=true, initial_capital=2000, commission_type=strategy.commission.percent, commission_value=0.2)
+
+
+//DATE AND TIME
+fromMonth = input(defval=1, title = "From Month", minval=1)
+fromDay = input(defval=1, title = "From Day", minval=1)
+fromYear = input(defval=2020, title = "From Year", minval=2014)
+
+toMonth = input(defval=6, title = "To Month", minval=1)
+toDay = input(defval=1, title = "To Day", minval=1)
+toYear = input(defval=2021, title = "To Year", minval=2014)
+
+
+
+//DEFINITIONS
+shortMa = sma(close, 13)
+longMa = sma(close, 200)
+
+
+//LOGIC
+timeInRange = (time > timestamp(fromYear, fromMonth, fromDay, 00, 00)) and (time < timestamp(toYear, toMonth, toDay, 23, 59)) 
+longSignal = crossover(shortMa, longMa) and timeInRange
+shortSignal = crossover(longMa, shortMa) and timeInRange
+
+//POSITIONS
+strategy.entry(id="longPosition", long=true, qty=0.1, when=longSignal)
+strategy.entry(id="shortPosition", long=false, qty=0.1, when=shortSignal)
